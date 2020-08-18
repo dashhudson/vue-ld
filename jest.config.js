de0d22aa9fd87c0acb60e991bf44023a041b8b25
Benjamin Frost <benjamin@dashhudson.com>
2020-08-18 08:49:54 -0300

    Tests actually run

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  jest: {
-    moduleFileExtensions: ['js', 'json', 'vue'],
-    transform: {
-      '.*\\.(vue)$': 'vue-jest',
-    },
-    collectCoverage: true,
-    collectCoverageFrom: ['**/*.{js,vue}', '!**/node_modules/**'],
-    coverageReporters: ['json', 'lcov', 'clover', 'html'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(svg|css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.jsx?$': '<rootDir>/node_modules/babel-jest',
   },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{js,vue}', '!**/node_modules/**'],
+  coverageReporters: ['json', 'lcov', 'clover', 'html'],
 };
