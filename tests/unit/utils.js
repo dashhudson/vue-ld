3f65922a7711b14bfede54252e0f2ae375511e45
Benjamin Frost <benjamin@dashhudson.com>
2020-08-18 14:28:03 -0300

    Add braces

--- tests/unit/utils.js
+++ tests/unit/utils.js
@@ -1,6 +1,7 @@
-export const ldClientReady = (wrapper) =>
+export const ldClientReady = (wrapper) => {
   new Promise((r) => {
     wrapper.vm.$ld.ldClient.on('ready', () => {
       r();
     });
   });
+};
