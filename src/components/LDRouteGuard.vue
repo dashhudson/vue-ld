95a6b05d71e309d51880e8c3d767b40663676a24
Benjamin Frost <60889846+benjamin-t-frost@users.noreply.github.com>
2021-02-03 12:17:43 -0400

    Fixed function prop wanring (#35)

--- src/components/LDRouteGuard.vue
+++ src/components/LDRouteGuard.vue
@@ -8,7 +8,7 @@ import ldRedirectMixin from '../mixins/ldRedirect';
 export default {
   mixins: [ldRedirectMixin()],
   props: {
-    component: { type: [Object, Promise], required: true },
+    component: { type: [Function, Object, Promise], required: true },
     requiredFeatureFlag: { type: String, required: true },
     to: { type: [String, Object], required: true },
   },
