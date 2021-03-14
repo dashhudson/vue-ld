dc2bfdaae9823f2793b5e1581be88d4b4ccbdc2e
Sitong Chen <comeonsitong@gmail.com>
2021-03-14 15:13:35 -0300

    Revert "add log"
    This reverts commit 6b4b102542607e2adc488d1b7411811307b0484a.

--- src/components/LDRouteGuard.vue
+++ src/components/LDRouteGuard.vue
@@ -18,7 +18,6 @@ export default {
       return this.$ld.ready && this.ldRedirectFlagValue;
     },
     importedComponent() {
-      console.log('kk');
       // Handle dynamically imported components
       if (!!this.component && typeof this.component.then === 'function') {
         return () => this.component;
