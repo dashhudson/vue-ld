18bf84436b85a2abb8d9ad338480d39f39f9b522
Graham Lounder <graham@dashhudson.com>
2022-07-06 10:17:23 -0300

    Upgraded to latest vue cli 4 to get new jest that works on macs by default

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-    '^.+\\.vue$': 'vue-jest',
-    '.+\\.(svg|css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    '^.+\\.jsx?$': '<rootDir>/node_modules/babel-jest',
-  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
