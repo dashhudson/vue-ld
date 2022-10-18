d9f785c2307eb24c1a2be84c20e3e1b1360622cc
Graham Lounder <graham@dashhudson.com>
2022-10-18 16:18:15 -0300

    Dynamicall use correct vue-jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,19 @@
+const { isVue3 } = require('vue-demi');
+
+const vueVersion = isVue3 ? '3' : '2';
+
+let vueJest = null
+try {
+  vueJest = require.resolve(`@vue/vue${vueVersion}-jest`)
+} catch (e) {
+  throw new Error(`Cannot resolve "@vue/vue${vueVersion}-jest" module. Please make sure you have installed "@vue/vue${vueVersion}-jest" as a dev dependency.`)
+}
+
 module.exports = {
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
   transform: {
-    '^.+\\.vue$': '@vue/vue2-jest',
+    '^.+\\.vue$': vueJest,
     '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|avif)$':
       'jest-transform-stub',
     '^.+\\.jsx?$': 'babel-jest',
